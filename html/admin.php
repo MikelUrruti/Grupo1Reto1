e86ff939764a8f241ae0e67f0a0b96a7f10519ff
Seilfox <73230206+Seilfox@users.noreply.github.com>
2021-10-19 10:01:34 +0200

    Update admin
    Falta cambiar la letra

--- html/admin.php
+++ html/admin.php
@@ -5,11 +5,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="../css/menuAdmin.css">
-    <link rel="stylesheet" href="../css/cssFooter.css">
-    <link rel="stylesheet" href="../css/normalizar.css">
+    <link rel="stylesheet" href="css/menuAdmin.css">
+    <link rel="stylesheet" href="css/cssFooter.css">
+    <link rel="stylesheet" href="css/normalizar.css">
+    <link rel="stylesheet" href="css/admin.css">
 </head>
 <body>
     <?php include ("plantillas/menuAdmin.html") ?>
+    <section>
+        <article>
+            <p>Bienvenido al panel de administración de Fix Point</p>
+            <img src="img/herramientasAdmin.png">
+        </article>
+    </section>
 </body>
 </html>
