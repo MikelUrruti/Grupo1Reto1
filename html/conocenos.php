b576c2473283cb027969956ff2c61335a2403c84
Seilfox <73230206+Seilfox@users.noreply.github.com>
2021-10-20 11:36:52 +0200

    MediaQuery conocenos
    Falta el tipo de letra

--- html/conocenos.php
+++ html/conocenos.php
@@ -9,25 +9,30 @@
     <link rel="stylesheet" href="css/casNav.css">
     <link rel="stylesheet" href="css/cssFooter.css">
     <link rel="stylesheet" href="css/conocenos.css">
+    <script src="JS/js.js"></script>
 </head>
 <body>
     <section id="conocenos">
         <article id="mision">
-            <h1>MISIÓN</h1><input type="checkbox" class="checkbox" id="mostrar1"/><label for="mostrar1">Ver más</label>
+            <h1>MISIÓN</h1><input type="checkbox" class="checkbox" name="checkbox" id="mostrar1"/><label class="verMas" id="ver1" for="mostrar1">Ver más</label>
             <p id="mParrafo">
-Como una comunidad de ávidos manitas, deportistas al aire libre y personas con conciencia ambiental, la Biblioteca de herramientas FixPoint, quiere provocar un cambio en el sistema de sobreproducción e ineficiencia resultante, la contaminación ambiental y la desigualdad (social). La misión de FixPoint es transformar la mentalidad en la sociedad, en la que la propiedad se elige sobre el acceso. Queremos hacer esto inspirando a la población y organizaciones a abrir una Biblioteca FixPoint en su vecindario para que la gente tenga acceso a cosas que solo necesitan de vez en cuando,en lugar de posesión. 
+Como una comunidad de ávidos manitas, deportistas al aire libre y personas con conciencia ambiental, la Biblioteca de herramientas FixPoint, quiere provocar un cambio en el sistema de sobreproducción e ineficiencia resultante, la contaminación ambiental y la desigualdad (social). 
+La misión de FixPoint es transformar la mentalidad en la sociedad, en la que la propiedad se elige sobre el acceso. 
+Queremos hacer esto inspirando a la población y organizaciones a abrir una Biblioteca FixPoint en su vecindario para que la gente tenga acceso a cosas que solo necesitan de vez en cuando,en lugar de posesión. 
             </p>
         </article>
 
         <article id="vision">
-            <h1>VISIÓN</h1><input type="checkbox" class="checkbox" id="mostrar2"/><label for="mostrar2">Ver más</label>
+            <h1>VISIÓN</h1><input type="checkbox" class="checkbox" name="checkbox" id="mostrar2"/><label class="verMas" id="ver2" for="mostrar2">Ver más</label>
             <p id="vParrafo">
-FixPoint es un proyecto que lucha por una economía circular. Esto significa que creemos en un sistema cerrado de materias primas, en el que los productores seguirán siendo los propietarios de las materias primas contenidas en los productos en el futuro. Para lograr esto, sin embargo, debemos facilitar que 'acceder a' sea más fácil, más barato y más divertido que 'poseer'. 
+FixPoint es un proyecto que lucha por una economía circular. 
+Esto significa que creemos en un sistema cerrado de materias primas, en el que los productores seguirán siendo los propietarios de las materias primas contenidas en los productos en el futuro. 
+Para lograr esto, sin embargo, debemos facilitar que 'acceder a' sea más fácil, más barato y más divertido que 'poseer'. 
             </p>
         </article>
 
         <article id="herramientas">
-            <h1>¿QUÉ ES UNA BIBLIOTECA DE HERRAMIENTAS?</h1><input type="checkbox" class="checkbox" id="mostrar3"/><label for="mostrar3">Ver más</label>
+            <h1>¿QUÉ ES UNA BIBLIOTECA DE HERRAMIENTAS?</h1><input type="checkbox" class="checkbox" name="checkbox" id="mostrar3"/><label class="verMas" id="ver3" for="mostrar3">Ver más</label>
             <p id="hParrafo">
 FixPoint es la primera Biblioteca de préstamo de España. Las Bibliotecas de herramientas funcionan como cualquier otra Biblioteca. Te conviertes en miembro y luego puedes tomar prestadas herramientas.
 
