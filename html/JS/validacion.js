00dc0b2fd90e02987a86d5dc8c00c39cfda7627c
Aitor <patopotex@gmail.com>
2021-10-22 12:56:32 +0200

    Validación (mejor)

--- html/JS/validacion.js
+++ html/JS/validacion.js
@@ -1,3 +1,5 @@
+var contador = 0;
+
 window.onload = function () {
 
     const nombre = document.getElementById("nombre");
@@ -6,49 +8,87 @@ window.onload = function () {
     const telefono = document.getElementById("telefono");
 
     nombre.addEventListener("keyup", validar);
-    apellidos.addEventListener("keyup", validarApellidos);
-    email.addEventListener("keyup", validar);
+    apellidos.addEventListener("keydown", validar);
+    email.addEventListener("keypress", validar);
     telefono.addEventListener("keyup", validar);
+
 }
 
-function validar(nombre, apellidos, email, telefono) {
+function validar(source) {
+    let s = source.target;
+    console.log(s);
+    console.log(contador);
+   if (!validarCampo(s)) {
+       s.style.border = "3px solid red";
+       contador++;
+    }
+    else {
+        s.style.border = "3px solid green";
+    }
+}
 
-    if(!validarNombre()){
-        nombre.style.border = "3px solid red";
+    function validarCampo(source) {
+        if (source == nombre || source == apellidos) {
+            if (source.value.length == 0 || !isNaN(source.value) || source.value == null || /^\s+$/.test(source.value)) {
+                return false;
+        }  else {return true;}
+    }
+        else if (source == email) {
+            if ( source.value.length == 0|| source.value == null ||  !(/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i.test(source.value))) {
+                return false; 
+            } else {return true;}
+    } 
+    else if (source == telefono) {
+
+    } 
+
+}    
+
         
-    }else{
-        nombre.style.border = "3px solid green";
-    }
-    if(!apellidos()){
-        apellidos.style.border = "3px solid red";
-    }
-}
+    // switch (xd) {
+    //     case nombre:
+    //       if (xd.value.length == 0 || !isNaN(xd.value) || xd.value == null || /^\s+$/.test(xd.value)) {
+    //         xd.style.border = "3px solid red";
+    //       }
+    //       else {
+    //         xd.style.border = "3px solid green";
+    //       }
+    //       alert("bien");
+    //       break;
+    //     default:
+    //         alert("mal");
+    //         break;
+    // }
 
-function validarNombre() {
-    // campo vacio - introducir numeros - campo nulo - espacios en blanco
-    if (nombre.value.length == 0 || !isNaN(nombre.value) || nombre.value == null || /^\s+$/.test(nombre.value)) {
-        return false;
-    } else {
-        return true;
-    }
-}
-function validarApellidos() {
-    // campo vacio - introducir numeros - campo nulo - espacios en blanco
-    if (apellidos.value.length == 0 || !isNaN(apellidos.value) || apellidos.value == null || /^\s+$/.test(apellidos.value)) {
-        apellidos.style.border = "3px solid red";
-    } else {
-        apellidos.style.border = "3px solid green";
-    }
-}
 
-function validarEmail(){
-    if(!(/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i.test(email.value)) ) {
-        email.style.border="3px solid red";
-    }else{
-        email.style.border="3px solid green";
-    }
-}
+    // return false;
 
-function validarTelefono(){
+// function validarNombre() {
+//     // campo vacio - introducir numeros - campo nulo - espacios en blanco
+//     if (nombre.value.length == 0 || !isNaN(nombre.value) || nombre.value == null || /^\s+$/.test(nombre.value)) {
+//         return false;
+//     } else {
+//         return true;
+//     }
+// }
+// function validarApellidos() {
+//     // campo vacio - introducir numeros - campo nulo - espacios en blanco
+//     if (apellidos.value.length == 0 || !isNaN(apellidos.value) || apellidos.value == null || /^\s+$/.test(apellidos.value)) {
+//         apellidos.style.border = "3px solid red";
+//     } else {
+//         apellidos.style.border = "3px solid green";
+//     }
+// }
+
+// function validarEmail(){
+//     if(!(/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i.test(email.value)) ) {
+//         email.style.border="3px solid red";
+//     }else{
+//         email.style.border="3px solid green";
+//     }
+// }
+
+// function validarTelefono(){
     
-}
+// }
+
