ee12d5f6a49b5af0a305008026affe84afda8a20
MikelUrruti <urruti00@gmail.com>
2021-10-25 23:08:07 +0200

    alguna operacion para db (sin acabar)

--- html/plantillasphp/dbconfig.php
+++ html/plantillasphp/dbconfig.php
@@ -1,8 +1,14 @@
 <?php 
 
-$host="sgbdreto1g1.cxmt9jhspupt.us-east-1.rds.amazonaws.com";
-$user="admin";
-$password="";
-$db="fixpoint";
+$array = array(
+
+    "host" => "sgbdreto1g1.cxmt9jhspupt.us-east-1.rds.amazonaws.com",
+    "user" => "admin",
+    "password" => "xqlesle2021",
+    "db" => "fixpoint",
+
+);
+
+return $array;
 
 ?>
--- html/plantillasphp/operacionesDb.php
+++ html/plantillasphp/operacionesDb.php
@@ -1,5 +1,39 @@
 <?php 
 
+$datosConexion = require_once("dbconfig.php");
 
+
+
+function select(string $consulta, string $ejecutar){
+
+    global $datosConexion;
+    // $datosConexion = require_once("dbconfig.php");
+    echo var_dump($datosConexion);
+
+    try {
+
+        $conexion = new PDO("mysql:host=".$datosConexion['host'].";port=3306;dbname=".$datosConexion['db'], $datosConexion['user'], $datosConexion['password']);
+        $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $consulta=$conexion->prepare($consulta);
+        $consulta->execute();
+        $resultados = $consulta->fetchAll();
+
+        foreach ($resultados as $valor) {
+            
+            eval($ejecutar);
+
+        }
+        
+    } catch (\Throwable $th) {
+        
+
+
+    }
+
+
+
+}
+
+echo select("select * from ","");
 
 ?>
