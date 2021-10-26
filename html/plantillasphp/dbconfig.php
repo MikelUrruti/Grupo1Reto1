f351015ee556292e6423d07b74874d0af0f5d510
MikelUrruti <urruti00@gmail.com>
2021-10-26 10:11:05 +0200

    Soporte para operaciones de insercion,modificacion y borrado de datos, incluyendo la posibilidad de utilizar una funcion para controlar los errores comunes

--- html/plantillasphp/dbconfig.php
+++ html/plantillasphp/dbconfig.php
@@ -5,7 +5,7 @@ $array = array(
     "host" => "sgbdreto1g1.cxmt9jhspupt.us-east-1.rds.amazonaws.com",
     "user" => "admin",
     "password" => "xqlesle2021",
-    "db" => "fixpoint",
+    "db" => "fixPoint",
 
 );
 
