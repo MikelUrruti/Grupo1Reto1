04348beb1859218ebfee8b4e576c14a444587bff
Aitor <patopotex@gmail.com>
2021-10-27 08:16:35 +0200

    s

--- html/JS/index.js
+++ html/JS/index.js
@@ -3,9 +3,15 @@
 
 window.onload = function () {
     const btnTest = document.getElementById("btnTest");
-    const txt = document.getElementById("txtEscribir");
+    const txt = document.getElementById("txtEscribir").innerHTML;
     btnTest.addEventListener("click", escribirTexto);
-    paso(txt);
+
+    
+
+    var primchar = ""+txt.textContent;
+    console.log(primchar.charAt(0));
+    var cambio=primchar.substring(1);
+    txt.innerHTML=cambio+primchar.charAt(0);
 }
 
 
@@ -16,24 +22,6 @@ function escribirTexto() {
     alert(txt);
     h1.appendChild(txt);
     document.body.appendChild(txt);
-    var primchar = ""+txt.textContent;
-    console.log(primchar.charAt(0));
-    var cambio=primchar.substring(1);
-    txt.innerHTML=cambio+primchar.charAt(0);
-}
-
-
-//Variables de los botones
-const txt = document.getElementById("texto");
-
-//Variable para el control de funciones
-var detener;
-
-function comenzar() {
-    detener = setInterval(paso, 100);
-}
-
-function paso(txt) {
     var primchar = ""+txt.textContent;
     console.log(primchar.charAt(0));
     var cambio=primchar.substring(1);
