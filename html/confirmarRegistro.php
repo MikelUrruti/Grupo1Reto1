48343a3a23bc9b404c4ce3eff480df538fc44bbc
MikelUrruti <75219918+MikelUrruti@users.noreply.github.com>
2021-11-02 13:25:10 +0100

    cambios final de hoy

--- html/confirmarRegistro.php
+++ html/confirmarRegistro.php
@@ -3,6 +3,10 @@
 require("plantillasphp/redirecciones.php");
 require("phpMailer/src/PHPMailer.php");
 require("phpMailer/src/SMTP.php");
+require("phpMailer/src/Exception.php");
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
 session_start();
 
@@ -17,14 +21,24 @@ if (!isset($_SESSION["usuario"]) || !isset($_SESSION["email"]) || !isset($_SESSI
     $max = strlen($pattern)-1;
     for($i=0;$i < 6;$i++) $_SESSION["codigoUsuario"] .= $pattern[mt_rand(0,$max)];
 
-    $mail=new PHPMailer();
-    $mail->isHTML(true);
-    $mail->CharSet = 'UTF-8';
-    $mail->addAddress($email, $name);
-    $mail->Body = " <h3>Hi $name,</h3><h4>Congrats!</h4>";
-    $mail->send();
-    $mail->ClearAllRecipients(); 
-
+    $to      = $_SESSION["email"]; // Send email to our user
+    $subject = 'Signup | Verification'; // Give the email a subject 
+    $message = '
+     
+    Thanks for signing up!
+    Your account has been created, you can login with the following credentials after you have activated your account by pressing the url below.
+     
+    ------------------------
+    Codigo: '.$_SESSION['codigoUsuario'].'
+    ------------------------
+     
+    Please click this link to activate your account:
+    http://www.yourwebsite.com/verify.php?email='.$_SESSION["email"].'&hash='.$_SESSION["email"].'
+     
+    '; // Our message above including the link
+                         
+    $headers = 'From:urruti00@gmail.com' . "\r\n"; // Set from headers
+    mail($to, $subject, $message, $headers); // Send our email
 }
 
 ?>
--- html/controladores/adminControladorUsuario.php
+++ html/controladores/adminControladorUsuario.php
@@ -4,14 +4,10 @@
     require_once("../plantillasphp/operacionesDb.php");
 
     session_start();
-
-    echo var_dump($_POST);
+    
 
     if (isset($_POST["Eliminar"])) {
         $accion = "Eliminar";
-    } elseif (isset($_POST["Buscar"])) {
-        // echo $_POST["Buscar"]." resultado";
-        $accion = "Buscar";
     } elseif (isset($_POST["Crear"])) {
         $accion = "Crear";
     } elseif (isset($_POST["Modificar"])) {
@@ -20,11 +16,11 @@
 
     if ($accion == "Crear") {
 
-        // redireccionar("../FormularioCrearUsuario.php");
+        redireccionar("../FormularioCrearUsuario.php");
 
     } elseif ($accion == "Modificar") {
         
-        // redireccionar("../FormularioModificarUsuario.php");
+        redireccionar("../FormularioModificarUsuario.php");
 
     } elseif ($accion == "Eliminar") {
 
@@ -54,27 +50,11 @@
 
         manipularDatoBD($consulta,$parametros);
 
-        // redireccionar("../adminUsuarios.php");
-
-    } elseif ($accion == "Buscar") {
-
-        $parametros = array();
-
-        for ($i=0; $i < 5; $i++) { 
-            
-            array_push($parametros,"%".$_POST["Buscar"]."%");
-
-        }
-
-        $_SESSION["filtrado"] = consultarDatoBD("select * from Usuario where usuario like ? or email like ? or nombre like ? or apellidos like ? or telefono like ?;",$parametros);
-
-        // echo var_dump($_SESSION["filtrado"]);
-
-        // redireccionar("../adminUsuarios.php");
+        redireccionar("../adminUsuarios.php");
 
     } else {
         
-        // redireccionar("../adminUsuarios.php");
+        redireccionar("../adminUsuarios.php");
 
     }
 
