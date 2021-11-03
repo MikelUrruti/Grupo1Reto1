bbe0c53cdbc8e3fe8d1a5b7e3c38ee6eea115d4b
JonGonzalezPallares <jongonzalezpallares0@gmail.com>
2021-11-03 09:40:40 +0100

    Update paginador.php

--- html/controladores/paginador.php
+++ html/controladores/paginador.php
@@ -1,74 +1,58 @@
-<?php 
-    require("plantillasphp/operacionesDb.php");  
-    require("cargarManuales.php");    
-    //La consulta para cargar los datos
-    $consulta = "select titulo from Manual;";
-    $parametros = array();
-    $filas = consultarDatoBD($consulta, $parametros);
-    $totalconsulta = count($filas);
-    echo ($totalconsulta);
+<?php
+require("plantillasphp/operacionesDb.php");
+require("cargarManuales.php");
+//La consulta para cargar los datos
+$consulta = "SELECT titulo FROM Manual;";
+$parametros = array();
+$filas = consultarDatoBD($consulta, $parametros);
+//Cantidad total de elementos que hemos recibido de la consulta
+$totalconsulta = count($filas);
 
-
-      if ($totalconsulta > 0) {
+//Si nos devuelve alguna consulta entra, si no, no
+if ($totalconsulta > 0) {
+    //$page = la pagina en la que nos situamos actualmente
     $page = false;
- 
+
     //examino la pagina a mostrar y el inicio del registro a mostrar
     if (isset($_GET["page"])) {
         $page = $_GET["page"];
     }
- 
+
+    //Lo que hace al cargar la pagina
     if (!$page) {
         $start = 0;
         $page = 1;
     } else {
         $start = ($page - 1) * 6;
     }
-    //calculo el total de paginas
-    // $total_page = ceil($totalconsulta / 6);
-    $total_pages = ceil($totalconsulta /6);
- 
-    //pongo el numero de registros total, el tamano de pagina y la pagina que se muestra
 
+    //Calculo el total de paginas
+    $total_pages = ceil($totalconsulta / 6);
+
+    //Pongo el numero de registros total, el tamano de pagina y la pagina que se muestra
     mostrarmanuales($filas);
- 
+
     //Inicio de la creacion de las bolitas
     echo '<article id="cantPag">';
     echo '<section id="movIzq">
         <img src="img/Paso.png" />
         </section>';
- 
+
+    //Comprobacion de los elementos que hay en la bd para crear las bolitas necesarias
     if ($total_pages > 1) {
-        if ($page != 1) {
-            echo '<section class="numPag" class="page-item">
-                <p>
-                    <a class="page-link" href="index.php?page='.($page-1).'"><span aria-hidden="true">&laquo;</span></a>
-                </p>
-            </section>';
-        }
- 
-        for ($i=1;$i<=$total_pages;$i++) {
+        //Estilo de las bolitas que marcan las paginas
+        for ($i = 1; $i <= $total_pages; $i++) {
             if ($page == $i) {
                 echo '
                 <section class="numPag" class="page-item active>
-                        <a class="page-link" href="#">'.$page.'</a>
+                    <a class="page-link" href="Manuales_lista.php">' . $page . '</a>
                 </section>';
             } else {
                 echo '
                 <section class="numPag" class="page-item>
-                    
-                        <a class="page-link" href="index.php?page='.$i.'">'.$i.'</a>
-                    
+                    <a class="page-link" href="Manuales_lista.php' . $i . '">' . $i . '</a>
                 </section>';
             }
-        }
- 
-        if ($page != $total_pages) {
-            echo '
-            <section class="numPag" class="page-item>
-                    <p>
-                    <a class="page-link" href="index.php?page='.($page+1).'"><span aria-hidden="true">&raquo;</span></a>
-                    </p>
-                </section>';
         }
     }
     echo '<section id="movDer">
