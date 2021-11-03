a8eec51ddff8a15a4a3396294c889988ebac5e08
Seilfox <73230206+Seilfox@users.noreply.github.com>
2021-11-03 09:39:13 +0100

    Update

--- html/controladores/paginador.php
+++ html/controladores/paginador.php
@@ -1,22 +1,21 @@
-<?php 
-    require("plantillasphp/operacionesDb.php");  
-    require("cargarManuales.php");    
-    //La consulta para cargar los datos
-    $consulta = "select titulo from Manual;";
-    $parametros = array();
-    $filas = consultarDatoBD($consulta, $parametros);
-    $totalconsulta = count($filas);
-    echo ($totalconsulta);
+<?php
+require("plantillasphp/operacionesDb.php");
+require("cargarManuales.php");
+//La consulta para cargar los datos
+$consulta = "select titulo from Manual;";
+$parametros = array();
+$filas = consultarDatoBD($consulta, $parametros);
+$totalconsulta = count($filas);
 
 
-      if ($totalconsulta > 0) {
+if ($totalconsulta > 0) {
     $page = false;
- 
+
     //examino la pagina a mostrar y el inicio del registro a mostrar
     if (isset($_GET["page"])) {
         $page = $_GET["page"];
     }
- 
+
     if (!$page) {
         $start = 0;
         $page = 1;
@@ -25,48 +24,48 @@
     }
     //calculo el total de paginas
     // $total_page = ceil($totalconsulta / 6);
-    $total_pages = ceil($totalconsulta /6);
- 
+    $total_pages = ceil($totalconsulta / 6);
+
     //pongo el numero de registros total, el tamano de pagina y la pagina que se muestra
 
-    mostrarmanuales($filas);
- 
+    mostrarmanuales($filas,$page);
+
     //Inicio de la creacion de las bolitas
     echo '<article id="cantPag">';
     echo '<section id="movIzq">
         <img src="img/Paso.png" />
         </section>';
- 
+
     if ($total_pages > 1) {
         if ($page != 1) {
             echo '<section class="numPag" class="page-item">
                 <p>
-                    <a class="page-link" href="index.php?page='.($page-1).'"><span aria-hidden="true">&laquo;</span></a>
+                    <a class="page-link" href="Manuales_lista.php?page='.($page-1).'"><span aria-hidden="true">&laquo;</span></a>
                 </p>
             </section>';
         }
- 
-        for ($i=1;$i<=$total_pages;$i++) {
+
+        for ($i = 1; $i <= $total_pages; $i++) {
             if ($page == $i) {
                 echo '
-                <section class="numPag" class="page-item active>
-                        <a class="page-link" href="#">'.$page.'</a>
+                <section class="numPag" class="page-item active">
+                        <a class="page-link" href="Manuales_lista.php">' . $page . '</a>
                 </section>';
             } else {
                 echo '
                 <section class="numPag" class="page-item>
                     
-                        <a class="page-link" href="index.php?page='.$i.'">'.$i.'</a>
+                        <a class="page-link" href="Manuales_lista.php?page=' . $i . '">' . $i . '</a>
                     
                 </section>';
             }
         }
- 
+
         if ($page != $total_pages) {
             echo '
-            <section class="numPag" class="page-item>
+            <section class="numPag" class="page-item">
                     <p>
-                    <a class="page-link" href="index.php?page='.($page+1).'"><span aria-hidden="true">&raquo;</span></a>
+                    <a class="page-link" href="Manuales_lista.php?page=' . ($page + 1) . '"><span aria-hidden="true">&raquo;</span></a>
                     </p>
                 </section>';
         }
@@ -77,3 +76,4 @@
     echo '</article>';
 }
 // }
+?>
