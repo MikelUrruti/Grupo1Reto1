c2db568440177ae3c62a03ea405dbc932f6bbeb5
JonGonzalezPallares <jongonzalezpallares0@gmail.com>
2021-11-04 12:29:55 +0100

    Update index.php
    ·Añadidos comentarios

--- html/index.php
+++ html/index.php
@@ -5,13 +5,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--Estilos-->
     <link rel="stylesheet" href="css/normalizar.css">
     <link rel="stylesheet" href="css/cssFooter.css">
     <link rel="stylesheet" href="css/cssNav.css">
     <link rel="stylesheet" href="css/style.css">
     <!--Favicon de las paginas-->
     <link rel="shortcut icon" href="img/Logo Header.png" />
-    <!--Para el tipo de letra-->
+    <!--Scripts-->
     <script src="JS/js.js"></script>
     <script src="JS/nav.js"></script>
     <script src="JS/index.js"></script>
