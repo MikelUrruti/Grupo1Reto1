a3777513204ed4df4814875bffcacb6f276dc164
JonGonzalezPallares <jongonzalezpallares0@gmail.com>
2021-11-09 12:15:31 +0100

    change img
    ·Imagen de paso final actualizada

--- html/plantillasphp/paginadorFunciones.php
+++ html/plantillasphp/paginadorFunciones.php
@@ -104,7 +104,7 @@
                 if ($page > 1) {
                     echo '
                     <section class="movIzq">
-                        <a href="'.$pagina.'?page=' . 1 . '"> <img src="img/Paso.png" /><img src="img/Paso.png" /></a>
+                        <a href="'.$pagina.'?page=' . 1 . '"> <img src="img/Pasodoble.png" /></a>
                     </section>';
                     echo '
                     <section class="movIzq">
@@ -169,7 +169,7 @@
                     </section>';
                     echo '
                     <section class="movDer">
-                        <a href="'.$pagina.'?page=' . $_SESSION["total_pages"] . '"> <img src="img/Paso.png" /><img src="img/Paso.png" /></a>
+                        <a href="'.$pagina.'?page=' . $_SESSION["total_pages"] . '"> <img src="img/Pasodoble.png" /></a>
                     </section>';
                 }
             }
