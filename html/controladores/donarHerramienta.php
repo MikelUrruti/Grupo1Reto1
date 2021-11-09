1650b166186fffbf426ce2b30958e73548b83c20
JonGonzalezPallares <jongonzalezpallares0@gmail.com>
2021-11-09 09:12:56 +0100

    Cambios varios
    ·Añadidos comentarios en "donarHerramientas"
    ·Subida la imagen

--- html/controladores/donarHerramienta.php
+++ html/controladores/donarHerramienta.php
@@ -9,13 +9,16 @@ session_start();
 //Ruta para almacenar las imagenes
 //realpath: ruta absoluta del fichero
 $rutaIndex = dirname(realpath(__FILE__));
+//La ruta de origen es de donde viene la foto
 $rutaOrigen = $_FILES['foto']['name'];
+//La ruta a la que queremos mandar la foto
 $rutaDestino = '../img/herramientas/' . $_FILES['foto']['name'];
 
 
 $_SESSION["ruta"] = $rutaDestino. " ------- ". $rutaOrigen;
 
 //Comprobaciones
+//isset => la variable esta declarada y no es nulo
 if (isset($_POST["herramienta"]) && isset($_POST["usuario"]) && isset($_POST["descripcion"]) && isset($_FILES['foto'])) {
     $correcto = true;
     if (!validarNombre($_POST["herramienta"])) {
@@ -29,24 +32,27 @@ if (isset($_POST["herramienta"]) && isset($_POST["usuario"]) && isset($_POST["de
         $correcto = false;
     }
 
+    //Si los tres pasos anteriores son correctos
     if ($correcto) {
         
         if (move_uploaded_file($rutaOrigen, $rutaDestino)) {
             $parametros = array($_SESSION["usuario"], $_POST["herramienta"], $_FILES['foto']['name'], $_POST["descripcion"]);
+            //Los signos de interrogacion, en este caso, cogen los valores de la primera variable que encuentre, en orden de creacion de esas subvariables
             $consulta = manipularDatoBD("insert into Donaciones values (?, ?, ?, ?)", $parametros);
 
             if (is_integer($consulta)) {
-
+                //Mensaje de error al insertar la herramienta
                 $_SESSION["errorGeneral"] = erroresInsertar($consulta, array("Usuario", "Herramienta", "Fichero", "Descripcion"));
                 echo $_SESSION["errorGeneral"];
 
             } else {            
-
+                //Mensaje cuando subes bien la herramienta
                 $_SESSION["exito"] = "Se ha enviado la herramienta de manera exitosa";
             
             }
             redireccionar("../donar.php");
         }else{
+            //Mensaje de error cuando la BD esta desconectada o algo asi
             $_SESSION["errorGeneral"] = "Error al subir la imágen. Intentelo más tarde";
         }
         redireccionar("../donar.php");
