6c7c330af9b21d22cff1c4ba17efc8b83b4c0e3c
MikelUrruti <urruti00@gmail.com>
2021-11-15 01:10:47 +0100

    herramientas a medias

--- html/controladores/buscarHerramienta.php
+++ html/controladores/buscarHerramienta.php
@@ -15,13 +15,13 @@
     
         }
     
-        $_SESSION["filtrado"] = consultarDatoBD("select nombre, fabricante, categoria, stock, foto, descripcion from Categoria where nombre like ? or descripcion like ?;",$parametros);
+        $_SESSION["filtrado"] = consultarDatoBD("select nombre, fabricante, categoria, stock, foto, descripcion from Herramienta where nombre like ? or descripcion like ?;",$parametros);
 
         $_SESSION["mantenerFiltrado"] = true;
 
         $_SESSION["filtradoPrimeraVez"] = true;
 
-        redireccionar("../adminCategorias.php");
+        redireccionar("../adminHerramientasAlmacen.php");
 
     }
 
