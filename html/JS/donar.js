23e1718052054573b56384f912b3b5caf073914e
Seilfox <73230206+Seilfox@users.noreply.github.com>
2021-11-15 10:11:04 +0100

    updates

--- html/JS/donar.js
+++ html/JS/donar.js
@@ -20,32 +20,3 @@ function validarCampo(source) {
         validar(source, validarNombre(source.target, cajaId));
     } 
 }
-
-//Para crear la etiqueta del textarea, que sirve para la descripcion
-function CrearDesc() {
-    //Asi creamos el textarea, pero no tiene estilos
-    var txtA = document.createElement("textarea");
-
-    var total = 500;
-
-    //Le damos un id y un nombre al textarea
-    txtA.id = "descripcion";
-    txtA.name = "descripcion";
-    //Le damos el placeholder y el tamaño maximo
-    txtA.placeholder = "Descripción de la herramienta..."
-    txtA.maxLength = total;
-
-    /*Si queremos que carge la etiqueta como "etiqueta" y no como texto,
-    tenemos que poner appendChild. 
-    Si por el contrario, ponemos innerHTML, solo va a ponerlo como texto*/
-    document.getElementById("desc").appendChild(txtA);
-}
-
-/*
-Sccript que guarda el url de la imagen seleccionada y se lo pasa a la imagen anterior
-para poder verla
-*/
-function cargarImg(event){
-    var imagen = document.getElementById("imgVision");
-    imagen.src = URL.createObjectURL(event.target.files[0]);
-}
